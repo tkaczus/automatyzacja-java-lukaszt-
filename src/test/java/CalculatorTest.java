50059dac44e1369aada6c9c6de74d337072473a7
tkaczus <lukasztkaczyk@gmail.com>
2017-12-04 11:29:37 +0100

    com tests

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -5,29 +5,64 @@ import org.junit.Test;
  * Created by Lukasz.Tkaczyk on 2017-12-04.
  */
 public class CalculatorTest {
+    private double a;
+    private double b;
+
     @Test
     public void canAddPositiveIntegers() {
-        Assert.assertEquals(Calculator.add(1, 2), 3, 0);
+        a = 1.0;
+        b = 2.0;
+        Assert.assertEquals("Adding 1+2", 3, Calculator.add(a, b), 0);
+    }
+
+    @Test
+    public void canAddNegativeIntegers() {
+        a = -11.0;
+        b = -2.0;
+        Assert.assertEquals("Adding -11+-2", -13, Calculator.add(a, b), 0);
     }
 
     @Test
     public void canSubtractPositiveIntegers() {
-        Assert.assertEquals(Calculator.subtract(3, 2), 1, 0);
+        a = 3.0;
+        b = 2.0;
+        Assert.assertEquals("Substract 3-2", 1, Calculator.subtract(a, b), 0);
+    }
+
+    @Test
+    public void canSubtractNegativeIntegers() {
+        a = -3.0;
+        b = -2.0;
+        Assert.assertEquals("Substract -3--2", -1, Calculator.subtract(a, b), 0);
     }
 
     @Test
     public void canDividePositiveIntegers() {
-        Assert.assertEquals(Calculator.divide(6, 2), 3, 0);
+        a = 6.0;
+        b = 4.0;
+        Assert.assertEquals("Divide 6/2", 1.5, Calculator.divide(a, b), 0);
     }
 
     @Test
     public void canDivideByZero() {
-        Assert.assertTrue( Double.isInfinite(Calculator.divide(6, 0)));
+        a = 6.0;
+        b = 0.0;
+        Assert.assertTrue("Divide 6 by 0", Double.isInfinite(Calculator.divide(a, b)));
+        Assert.assertTrue("Divide 6 by 0", Calculator.divide(a, b)==Double.POSITIVE_INFINITY);
     }
 
     @Test
     public void canMultiplyPositiveIntegers() {
-        Assert.assertEquals(Calculator.multiply(3, 4), 12, 0);
+        a = 3.0;
+        b = 4.0;
+        Assert.assertEquals("Multiply 3*4", 12, Calculator.multiply(a, b), 0);
+    }
+
+    @Test
+    public void canMultiplyNegativeIntegers() {
+        a = -3.0;
+        b = 4.0;
+        Assert.assertEquals("Multiply -3*4", -12, Calculator.multiply(a, b), 0);
     }
 
 }
